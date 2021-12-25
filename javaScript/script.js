39bb1d78062513f65ebb43472880198983d48e5a
Yerzhanov Beksultan <200103513@stu.sdu.edu.kz>
2021-12-25 15:50:22 +0600

    lmost ended

--- javaScript/script.js
+++ javaScript/script.js
@@ -27,5 +27,38 @@ $(document).ready(function() {
 window.addEventListener("scroll", function(){
     var header = document.querySelector(".header");
     header.classList.toggle("sticky", window.scrollY > 500);
-})
+});
+
+        var proffession = 'Study (We hope)';
+        var ind1 = 0;
+        var bool = true;
+        var ind2 = 10;
+        var prof = ['Study (We hope)','Chill with your friends', 'Have lunch (ofc)', 'Drink tea with teachers', 'Have fun', "Be SDUdent", "Take part in activities"];
+
+        setInterval(function() {
+                if(bool){
+                    document.getElementById('activity').innerHTML = prof[ind1].substring(0, ind2);
+                    ind2--;
+                    if(ind2 < 0){
+                      ind1++;
+                      if(ind1 == prof.length){
+                        ind1 = 0;
+                      }
+                      ind2 = 1;
+                      bool = false;
+                    }
+                  }
+                  else {
+           
+                    document.getElementById('activity').innerHTML = prof[ind1].substring(0, ind2);
+                    ind2++;
+                    if(ind2 == prof[ind1].length+1){
+               
+                      ind2 = prof[ind1].length-1;
+                      bool = true;
+                    }
+                  }
+            
+          }, 150);
+  
 
